3c435faf4d742ed67adc7d06adf79f6ea510fd4d
Cailtin Duncan <cailtinduncan@caitlins-mbp.attlocal.net>
2019-07-18 17:09:10 -0400

    Completed Challenge 2

---
+++ TileMap (challenge 2)/Assets/Scripts/Transformer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transformer : MonoBehaviour
+{
+
+
+    public float speed;
+    Vector3 pointA = new Vector3(6.5f, -3.8f, 0.6f);
+    Vector3 pointB = new Vector3(15.5f, -3.8f, 0.6f);
+
+
+
+    void Update()
+    {
+
+        transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+    }
+}
